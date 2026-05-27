67ea199e27ebe8d9a572a10d28f4f9023c7e5f1f
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 08:11:49 +0100

    feat: implement module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "admin_account_id" {
+  description = "The AWS account ID of the FMS administrator."
+  value       = try(aws_fms_admin_account.this[0].id, "")
+}
+
 output "enabled" {
   description = "Whether the module is enabled."
   value       = local.enabled
